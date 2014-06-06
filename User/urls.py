dcf999578c0e320cbbc48f810f90999c54fa4fe2
Abhas Mittal <mittalabhas1@gmail.com>
2014-06-06 18:13:52 +0530

    feat(app/User): Created views, templates, gitignore for .pyc added

--- User/urls.py
+++ User/urls.py
@@ -4,7 +4,6 @@ from User import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^(?P<username>\d+)/$', views.HomeView.as_view(), name='home'),
 )
--- User/views.py
+++ User/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from User.models import User, UserDetails
+
+class IndexView(TemplateView):
+	"""
+	Shows the login form for the user.
+	"""
+	template_name = "User/index.html"
+
+class HomeView(generic.DetailView):
+	model = UserDetails
+	template_name = "User/home.html"
+
+def login(request):
+	"""
+	Controls the login mechanism.
+	Redirects to home page for successs, otherwise shows the login form again with an error message.
+	"""
+	try:
+		user = User.objects.get(username=request.POST['username'], password=request.POST['password'])
+	except User.DoesNotExist:
+		return render(request, 'User/index.html', {
+			'error_message': "Invalid Credentials !",
+		})
+	else:
+		return HttpResponseRedirect(reverse(
+			'user:home', args=(user.id,)
+		))
